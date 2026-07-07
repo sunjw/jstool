0dbe67d23b9beabf1eed5100cfb3b292ce1cfba0
Sun Junwen <sunjw8888@gmail.com>
2026-07-07 16:05:32 +0800

    vsc: Bump version to 26.7.20.

--- JSToolJS/jsparser.js
+++ JSToolJS/jsparser.js
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 'use strict';
 
-const VERSION = "25.11.16.0";
+const VERSION = "26.7.20";
 
 function StringReplaceAll(string, target, replace) {
     return string.replace(new RegExp(target, 'g'), replace);
