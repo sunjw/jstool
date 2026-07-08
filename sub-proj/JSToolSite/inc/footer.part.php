e1222eae9a7d5f92aaf3c9fa47e8ecd83dcdcff3
Sun Junwen <sunjw8888@gmail.com>
2026-07-08 11:07:14 +0800

    site: Update footer.

--- sub-proj/JSToolSite/inc/footer.part.php
+++ sub-proj/JSToolSite/inc/footer.part.php
@@ -4,7 +4,6 @@
 <div>
     <p>Except where otherwise noted, content on this site is licensed under the
 <a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">Creative Commons Attribution Share-Alike License v3.0</a>.</p>
-    <p>2010- <a href="http://www.sunjw.us/blog" target="_blank">Sun Junwen</a>&nbsp;|&nbsp;sunjw8888 at gmail.com&nbsp;|&nbsp;<a href="https://github.com/sunjw/fhash" target="_blank">fHash</a>&nbsp;|&nbsp;<a href="http://weibo.com/nusjw" target="_blank" title="Follow me on Weibo"><img src="../imgs/weibo-small.png" alt="Follow me on Weibo"/></a>&nbsp;<a href="http://www.twitter.com/sunjw" target="_blank" title="Follow me on Twitter"><img src="../imgs/twitter-small.png" alt="Follow me on Twitter"/></a>&nbsp;|&nbsp;
-        <a href="https://github.com/sunjw/jstool" target="_blank"><img src="../imgs/GitHub-Mark-32px.png"></a>
+    <p>2010- <a href="http://www.sunjw.us/blog" target="_blank">Sun Junwen</a>&nbsp;|&nbsp;sunjw8888 at gmail.com&nbsp;|&nbsp;<a href="https://github.com/sunjw/fhash" target="_blank">fHash</a>&nbsp;|&nbsp;<a href="https://github.com/sunjw/jstool" target="_blank"><img src="../imgs/GitHub-Mark-32px.png"></a>
     </p>
 </div>
